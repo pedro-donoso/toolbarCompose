6a91a471d7bff27016494abe98830eb3070f904d
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-07 23:41:59 -0300

    agrego background toolbar

--- app/src/main/java/com/example/columnapplication/MainActivity.kt
+++ app/src/main/java/com/example/columnapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.columnapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,7 +18,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -30,6 +35,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
 import coil.compose.rememberAsyncImagePainter
 
@@ -37,23 +43,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         supportActionBar?.title = "LazyColumn y LazyRow"
-
         setContent {
-            App()
+            ViewContainer()
         }
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun App() {
+fun ViewContainer() {
+    Scaffold(
+        content = { Content() }
+    )
+}
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun Toolbar() {
+    TopAppBar(
+        title = { Text(text = "LazyColumn & LazyRow", color = colorResource(id = R.color.white))},
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = colorResource(id = R.color.background)
+        )
+    )
+}
+
+@Composable
+fun Content() {
     var counter by rememberSaveable {
         mutableIntStateOf(0)
     }
-
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -68,7 +90,6 @@ fun App() {
                 contentDescription = "Logo",
                 contentScale = ContentScale.Crop
             )
-
             Row(
                 modifier = Modifier
                     .padding(18.dp)
@@ -78,14 +99,11 @@ fun App() {
                     contentDescription = "Like",
                     modifier = Modifier.clickable { counter++ }
                 )
-
                 Text(text = counter
                     .toString(),
                     color = Color.White,
                     modifier = Modifier.padding(start = 4.dp))
-
             }
-
             Text(
                 text = "Bienvenido",
                 fontSize = 25.sp,
@@ -93,9 +111,7 @@ fun App() {
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center
             )
-
             Spacer(modifier = Modifier.height(10.dp))
-
             Text(
                 text = "Suscríbete",
                 fontSize = 20.sp,
@@ -103,9 +119,7 @@ fun App() {
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center
             )
-
             Spacer(modifier = Modifier.height(10.dp))
-
             Text(
                 text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa",
                 fontSize = 15.sp,
@@ -115,14 +129,12 @@ fun App() {
                     .padding(15.dp),
                 textAlign = TextAlign.Justify
             )
-
             LazyRow(
                 horizontalArrangement = Arrangement.spacedBy(10.dp),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 15.dp, end = 15.dp)
             ) {
-
                 items(10) { index ->
                     Image(
                         painter = rememberAsyncImagePainter("https://picsum.photos/id/${index + 1}/200/300"),
@@ -134,9 +146,7 @@ fun App() {
                     )
                 }
             }
-
             Spacer(modifier = Modifier.height(10.dp))
-
             Text(
                 text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa",
                 fontSize = 15.sp,
@@ -146,7 +156,6 @@ fun App() {
                     .padding(15.dp),
                 textAlign = TextAlign.Justify
             )
-
             Spacer(modifier = Modifier.height(50.dp))
         }
     }
